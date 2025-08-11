d1daf02b7c11d4cb0718cd78e299fc96a0e0f623
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2025-08-11 09:06:54 +0530

    first commit

---
+++ Basic/Imageread.py
@@ -0,0 +1,8 @@
+# Python code to read image
+import cv2
+
+# To read image from disk, we use
+# cv2.imread function, in below method,
+img = cv2.imread("E:\OPEN-CV\Basic\wallpaper.jpg", cv2.IMREAD_COLOR)
+
+print(img)
