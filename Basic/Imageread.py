cbfaec3f39d16f87191a22b86ccd51fb5a7e1d4e
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2025-08-11 13:16:03 +0530

    cv

--- Basic/Imageread.py
+++ Basic/Imageread.py
@@ -4,5 +4,8 @@ import cv2
 # To read image from disk, we use
 # cv2.imread function, in below method,
 img = cv2.imread("E:\OPEN-CV\Basic\wallpaper.jpg", cv2.IMREAD_COLOR)
+cv2.imshow(img)
+cv2.waitKey(0)
 
-print(img)
+
+cv2.destroyAllWindows()
